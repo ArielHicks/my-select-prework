a18fe2b1d9923c0513692511fcfad46b0cae0c7f
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-02 15:27:44 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection << yield(array[i])
+    i += 1
+  end
 end
+
+my_collect(list) {|i| i.split(" ").first}
