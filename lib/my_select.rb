f6d8ee27415324c1adbbd25851831da55273af98
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-02 15:29:27 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,42 @@
-def my_select(collection)
-  i = 0
-  name_select = []
-  while i < collection.length
-    name_select << yield(array[i])
-    i += 1
-  end
-end
+nums = [1, 2, 3, 4, 5] 
 
-my_select(list) {|i| i.split(" ").first}
+def my_select(collection) 
+  empty_arr = [] 
+  
+  i=0 
+  while i < collection.length
+  empty_arr.push yield(collection[i]) 
+  i += 1 
+end 
+
+  empty_arr.compact 
+end 
+
+
+my_select(nums) do |num| 
+  if num.even?
+    num 
+  end 
+end 
+
+nums = [1, 2, 3, 4, 5] 
+
+def my_select(array) 
+  i =0 
+  select = [] 
+  while i < array.length 
+    if (yield(array[i]))
+      # select.push(array[i])OR 
+      select << array[i] 
+    end 
+    i += 1 
+  end 
+  select 
+end 
+
+my_select(nums) do |num| 
+  num.even? 
+end 
+
+
+
